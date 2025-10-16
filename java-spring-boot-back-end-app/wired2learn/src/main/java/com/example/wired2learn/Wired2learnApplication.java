1656b5a10fc7dcb0dbb9c9451ffe1dc896db8fdd
lakshek <email2shek@gmail.com>
2025-10-16 08:05:36 -0500

    init: generated Spring Boot project using Initializr

---
+++ java-spring-boot-back-end-app/wired2learn/src/main/java/com/example/wired2learn/Wired2learnApplication.java
@@ -0,0 +1,13 @@
+package com.example.wired2learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wired2learnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wired2learnApplication.class, args);
+	}
+
+}
---
+++ java-spring-boot-back-end-app/wired2learn/src/test/java/com/example/wired2learn/Wired2learnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wired2learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wired2learnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
