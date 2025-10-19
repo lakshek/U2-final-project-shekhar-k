81db69c30912a3ca48bb964edd0d8d9e6ec1a3c4
lakshek <email2shek@gmail.com>
2025-10-19 18:41:59 -0500

    feat: added Global Exception Handler

--- java-spring-boot-back-end-app/wired2learn/src/main/java/com/example/wired2learn/exception/GlobalExceptionHandler.java
+++ java-spring-boot-back-end-app/wired2learn/src/main/java/com/example/wired2learn/exception/GlobalExceptionHandler.java
@@ -1,5 +1,22 @@
 package com.example.wired2learn.exception;
 
+// Import HTTP status code
+import org.springframework.http.HttpStatus;
+
+// Import Spring object that represents an HTTP response (body + headers + status code)
+import org.springframework.http.ResponseEntity;
+
+// Indicates this is a global exception handler
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+//
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
 }
