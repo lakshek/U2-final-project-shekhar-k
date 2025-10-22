21f162dafdb889870a3e9064bfe87b328d667a33
lakshek <email2shek@gmail.com>
2025-10-21 20:44:59 -0500

    feat: added Topic entity model

--- java-spring-boot-back-end-app/wired2learn/src/main/java/com/example/wired2learn/model/Topic.java
+++ java-spring-boot-back-end-app/wired2learn/src/main/java/com/example/wired2learn/model/Topic.java
@@ -1,4 +1,103 @@
 package com.example.wired2learn.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "topics")
 public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Many topics are in one category
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "topic_title", nullable = false)
+    private String title;
+
+    @Column(name = "topic_content", columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name = "topic_image_url", length = 500)
+    private String imageUrl;
+
+    @Column(name = "topic_is_protected", nullable = false)
+    private boolean isProtected;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+
+    // Constructors
+    public Topic() {
+    }
+
+    public Topic(Category category, String title, String content, String imageUrl, boolean isProtected) {
+        this.category = category;
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.isProtected = isProtected;
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isProtected() {
+        return isProtected;
+    }
+    public void setProtected(boolean aProtected) {
+        isProtected = aProtected;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
 }
