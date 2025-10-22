91783df7f4f90bf3ae793aa877d6c3ae5b81d3db
lakshek <email2shek@gmail.com>
2025-10-21 20:23:04 -0500

    feat: added Category entity model

--- java-spring-boot-back-end-app/wired2learn/src/main/java/com/example/wired2learn/model/Category.java
+++ java-spring-boot-back-end-app/wired2learn/src/main/java/com/example/wired2learn/model/Category.java
@@ -1,4 +1,65 @@
 package com.example.wired2learn.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_name", nullable = false)
+    private String name;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Topic> topics;
+
+    // Constructors
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
 }
